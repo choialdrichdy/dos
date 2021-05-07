309157dbb83b12400e8dfe7a32c58305be4b72c0
Aldrich Choi <choi.aldrich@gmail.com>
2021-05-07 23:02:11 +0900

    Initial Commit

---
+++ config.py
@@ -0,0 +1,37 @@
+OUTPUT_LOCATION = "/opt/oppo"
+
+INPUT_FILE = "/opt/oppo/Sales-Stock-Analysis-3.xlsx"
+
+DISTRIBUTOR_WATCHLIST = [
+    'comworks',
+    'Cellboy Sales and Services Corp',
+    'rulls'
+]
+
+MODELS_WATCHLIST = [
+    'A12 (3+32G)',
+    'A12 (4+64G)',
+    'A12 SMART (3+32G)',
+    'A53 (4+64G)',
+    'A54 4G (6+128G)',
+    'A74 4G (6+128G)',
+    'A92 (8+128G)',
+    'A94 (8+128G)',
+    'OPPO A15 3GB',
+    'OPPO A15s 4GB',
+    'OPPO RENO5 4G',
+    'OPPO RENO5 5G',
+    'RENO3 PRO (8+256G)',
+    'RENO4'
+]
+
+LOCATIONS = [
+    'PH DUMMY STORES',
+    'NCR',
+    'CENTRAL LUZON',
+    'SOUTH LUZON',
+    'EAST NORTH LUZON',
+    'VISAYAS',
+    'MINDANAO',
+    'WEST NORTH LUZON'
+]
